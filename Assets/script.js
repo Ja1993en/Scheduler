19981e26179cb630fd9b3a16dfe5133cbb6b7710
Basquiat <jlnmcneal@gmail.com>
2022-04-06 03:28:02 -0500

    Read me

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,6 @@
 // pull some elements in for every hour 
 var workTime = [9,10,11,12,13,14,15,16,17];
 var info = document.querySelector('#input');
-var save = document.querySelector('.saveBtn')
 
 
 
@@ -11,40 +10,61 @@ $("#currentDay").text(today.format("MMM Do, YYYY"));
 console.log(today);
 
 hourColor = () => {
+    // Loops through each element 
+    $('.description').each(function(i,obj){
 var currentHour = moment().format('H');
-if (workTime > currentHour){
-    $('.description').css('background-color', 'green');
-} else if (workTime < currentHour) {
-    $('.description').css('background-color', 'grey');
+if ((i+9) > currentHour){
+    $(this).css('background-color', 'green');
+} else if ((i+9) < currentHour) {
 
-} else if (workTime == currentHour){
-    $('.description').css('background-color', 'red');
+    $(this).css('background-color', 'grey');
+
+} else if ((i+9) == currentHour){
+    $(this).css('background-color', 'red');
 
 }
+    });
 }
+
+//Get items from local storage and displays inputs
+$("#09 .description").val(localStorage.getItem("09"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
 
 infoDisplay = () => {
-    var hourInput = localStorage.getItem("event-input");
-    info.textContent = hourInput;
+    // var hourInput = localStorage.getItem("event-input");
+    // info.textContent = hourInput;
    
 
 }
-save.addEventListener("click", function(event) {
-    event.preventDefault();
 
-    var newInfo = document.querySelector('#input').value;
+// Save inputs into local storage
+ $(".saveBtn").click(function() {
 
-    localStorage.setItem("event-input", newInfo);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id")
+
+    //put in local storage
+    localStorage.setItem(time, text);
+    // localStorage.setItem("event-input", info.value);
+
     infoDisplay();
-}
+    }
+    
 );
 
 
 
 
 // document.querySelector("saveBtn").addEventListener("click")
-
+infoDisplay();
 hourColor();
 
 
